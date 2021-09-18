2751e7e025bc945645f32bbc2d58df67b8938a36
Miljan Djordjevic <miljandv@gmail.com>
2021-09-18 04:13:07 -0500

    good flask localy

--- Google Cloud/hello.py
+++ Google Cloud/hello.py
@@ -1,3 +1,11 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
 if __name__ == "__main__":
-    print(3)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
